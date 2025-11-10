18b70eceb3a07bee4a21a50ce89bd499339fa8e7
PrasannahRam <rprasu2005@gmail.com>
2025-11-10 17:46:11 +0530

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <section id="about" className="about" style={{marginBottom: 0 ,paddingBottom:0}}>
       <h2>About Me</h2>
-      <p>I am a passionate developer who loves to create efficient and scalable applications. I specialize in React and full-stack development.</p>
+      <p>I’m an undergraduate student at the University of Moratuwa, currently pursuing my first year in Electronic and Telecommunication Engineering. I’m passionate about technology, innovation, and building solutions that make everyday tasks easier and more efficient. I enjoy exploring new ideas, learning continuously, and applying my knowledge to real-world projects that have a meaningful impact.</p>
     </section>
   )
 }
